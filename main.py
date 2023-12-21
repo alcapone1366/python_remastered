e04790e555657e7a2275b6d7a189250b7cc36f67
nero-ot <babapour.sadegh.dev@gmail.com>
2023-12-20 23:41:24 -0700

    from vm called production

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # sample main code . Testing new local copy with the repo copy
 # in github. basically sanity check the connection
-
+import datetime
 
 if __name__ == "__main__":
     print("Hello, Friend!", '\n')
 
     for i in range(10):
-        print(i)
+        print(i, datetime.datetime.now())
